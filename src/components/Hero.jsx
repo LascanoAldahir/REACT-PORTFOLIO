48124002e37b7a9a0e8d403466ab79341926e848
LascanoAldahir <aldahirlascano@gmail.com>
2024-11-27 18:51:19 -0500

    about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import profilePic from "../assets/aldahirProfile.jpeg"
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 px-14 lg:mb-35">
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
@@ -12,14 +12,14 @@ const Hero = () => {
                     </h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4x1 tracking-tight text-transparent">Full Stack Developer
                     </span> 
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter" >{HERO_CONTENT}                        
+                     <p className="my-2 max-w-xl py-6 font-light tracking-tighter" >{HERO_CONTENT}                        
                     </p>
                 </div>
             </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-            <div className="felx justify-center">
-                <img src={profilePic} alt="Aldahir Lascano"/>
-            </div>
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center">
+                    <img src={profilePic} alt="Aldahir Lascano"/>
+                </div>
         </div>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ import { FaInstagram } from "react-icons/fa"
 
 const Navbar = () => {
   return (
-    <nav className= " mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items center">
-            <img src={logo} alt="logo" className="w-[60px] h-[32px]"/>
+    <nav className= "mb-5 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            < img src={logo} alt="logo"/>
         </div>
         
-        <div className="m-8 flex items-center justify-center gap-4 text-2x1 ">
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
             <FaLinkedin/>
             <FaGithub/>
             <FaInstagram/>
