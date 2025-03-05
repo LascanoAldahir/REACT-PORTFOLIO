5302d6086c19ca8934bfc9e1dfe406e49aaf8cb3
LascanoAldahir <aldahirlascano@gmail.com>
2025-03-04 20:54:21 -0500

    Rangos 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                         initial="hidden"
                         animate="visible"
                    
-                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4x1 tracking-tight text-transparent">Full Stack Developer
+                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4x1 tracking-tight text-transparent">Software Developer
                     </motion.span> 
                      <motion.p variants={container (1)}
                         initial="hidden"
